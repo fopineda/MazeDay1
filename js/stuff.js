14b1eb0e56699743ac23a2f889dbc39ad6e4a091
Felipe Pineda <fpineda@live.unc.edu>
2017-05-01 01:23:01 -0400

    Gong

--- js/stuff.js
+++ js/stuff.js
@@ -35,7 +35,7 @@ var harp1  = new Howl({
     src: ['instruments/Harp1.wav']
 })
 
-var harp1  = new Howl({
+var gong1  = new Howl({
     src: ['instruments/gong1.wav']
 })
 
@@ -119,7 +119,7 @@ function animalsQuiz(index,score){
 function instrumentsQuiz(index,score){
     var drumsQuestion = new SoundQuestion("What's the instrument that plays this sound?", ["Drums", "Drum", "I don't know"], drums1);
     var harpsQuestion = new SoundQuestion("What's the instrument that plays this sound?", ["Harp", "Harps", "I don't know"], harp1);
-    var gongsQuestion = new SoundQuestion("What's the instrument that plays this sound?", ["Gong", "A Harps", "I don't know"], harp1);
+    var gongsQuestion = new SoundQuestion("What's the instrument that plays this sound?", ["Gong", "A Harps", "I don't know"], gong1);
     var questionsList = [
         drumsQuestion,
         harpsQuestion,
